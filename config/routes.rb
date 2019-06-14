55a0bde389050099c07fdae3acdab7e04a611dee
EC2 Default User <ec2-user@ip-172-31-37-97.us-east-2.compute.internal>
2019-06-14 09:12:26 +0000

    retry

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  get 'topics/new'
+  get 'sessions/new'
+  get 'pages/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/help'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+  resources :comments
+  
+  get "favorites/index"
+  post "/favorites", to: "favorites#create"
+  delete "/favorites", to: "favorites#destroy"
+  
+end
+ 
